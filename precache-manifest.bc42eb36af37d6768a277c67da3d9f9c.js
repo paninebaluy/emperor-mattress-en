224c8c7e9293e51172d92d88da7209a1bfeed181
Natalia Dolgushina <paninebaluy@gmail.com>
2020-07-27 21:11:47 +0300

    Updates

---
+++ precache-manifest.bc42eb36af37d6768a277c67da3d9f9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8005976b82d724a1f675fd0623d76b",
+    "url": "/emperor-mattress-ru/index.html"
+  },
+  {
+    "revision": "0aaf635e396fe525df02",
+    "url": "/emperor-mattress-ru/static/css/main.bfdd84b2.chunk.css"
+  },
+  {
+    "revision": "78e88ee31c5cb2b713e5",
+    "url": "/emperor-mattress-ru/static/js/2.39cf9988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emperor-mattress-ru/static/js/2.39cf9988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aaf635e396fe525df02",
+    "url": "/emperor-mattress-ru/static/js/main.98903ce3.chunk.js"
+  },
+  {
+    "revision": "5423e1b3e5cce39ec02a",
+    "url": "/emperor-mattress-ru/static/js/runtime-main.3be614bf.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/emperor-mattress-ru/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "214066000eefee88185c190ee74810df",
+    "url": "/emperor-mattress-ru/static/media/Robotocondensed.21406600.woff"
+  },
+  {
+    "revision": "9c64c02a0b1e5e9ebea64953200dd7b1",
+    "url": "/emperor-mattress-ru/static/media/bed.9c64c02a.svg"
+  }
+]);
